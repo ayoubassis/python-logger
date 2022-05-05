be34cc4cea61e1f721f46bae44e43e21be8f2f0b
blurry-mood <assis.ayoub@gmail.com>
2022-05-05 18:20:27 +0200

    add simple (py) tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,14 @@
 from lgg import get_logger
 
-logger = get_logger()
+
+def test_import():
+    get_logger()
+
+
+def test_logging():
+    logger = get_logger()
+
+    logger.info('This is an info message')
+    logger.debug('Debugging message')
+    logger.error('Error message')
+    logger.warning('File not found! An empty one is created')
